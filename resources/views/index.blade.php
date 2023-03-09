c60e31526700b873d7b849fda8dc13d6f78b15f9
LuDon89 <daudonlucas@gmail.com>
2023-03-09 10:08:13 +0100

    add index header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,13 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+            Cute Cats Blog
+        </h1>
+        <p>
+            Un blog pour poster des photos de chats mignons
+        </p>
+    </x-slot>
+
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <p>Yo la mif</p>
     </div>
